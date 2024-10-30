221bd5bc8844895512ab00671d9456f82f76824b
sandevD <sandev.net@gmail.com>
2024-10-30 23:09:03 +0530

    ✨ Add Treatment model class with properties and methods

---
+++ model/Treatment.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Treatment {
+    private String name;
+    private double price;
+
+    public Treatment(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
