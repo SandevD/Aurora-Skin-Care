2dbd9a69f6a95f9d8e148d8b633000aa4e2c4820
sandevD <sandev.net@gmail.com>
2024-10-30 23:09:41 +0530

    ✨ Add Main class to initialize and run AppointmentController

---
+++ Main.java
@@ -0,0 +1,18 @@
+import controller.AppointmentController;
+
+public class Main {
+
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
+    public static void main(String[] args) {
+        AppointmentController controller = new AppointmentController();
+        controller.run();
+    }
+}
