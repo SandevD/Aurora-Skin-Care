bffc85887cddf7e0740bca225d9deba7329e59ca
sandevD <sandev.net@gmail.com>
2024-11-08 18:14:59 +0530

    ✨ Update invoice generation to include registration fee and enhance output formatting

--- model/Invoice.java
+++ model/Invoice.java
@@ -8,17 +8,23 @@ public class Invoice {
     }
 
     public void generateInvoice() {
-        double subtotal = appointment.getTreatment().getPrice();
+        double subtotal = appointment.getTreatment().getPrice() + appointment.getRegistrationFee();
         double tax = subtotal * 0.025;
         double total = Math.round((subtotal + tax) * 100.0) / 100.0;
 
-        System.out.println("\n--- Invoice ---");
-        System.out.println("Patient: " + appointment.getPatient().getName());
-        System.out.println("Appointment ID: " + appointment.getAppointmentId());
-        System.out.println("Treatment: " + appointment.getTreatment().getName());
-        System.out.println("Subtotal: " + subtotal);
-        System.out.println("Tax: 2.5% (" + Math.round(tax * 100.0) / 100.0 + ")");
-        System.out.println("Total: " + total);
-        System.out.println("-----------------\n");
+        System.out.println("\n+--------------------- Invoice ----------------------+");
+        System.out.format("| %-17s : %-30s |%n", "Appointment ID", appointment.getAppointmentId());
+        System.out.format("| %-17s : %-30s |%n", "Day", appointment.getDay());
+        System.out.format("| %-17s : %-30s |%n", "Time", appointment.getTime());
+        System.out.format("| %-17s : %-30s |%n", "Patient", appointment.getPatient().getName());
+        System.out.format("| %-17s : %-30s |%n", "Treatment", appointment.getTreatment().getName());
+        System.out.format("| %-17s : %-30s |%n", "Treatment Price", appointment.getTreatment().getPrice());
+        System.out.format("| %-17s : %-30s |%n", "Registration Fee", appointment.getRegistrationFee());
+        System.out.println("+----------------------------------------------------+");
+        System.out.format("| %-17s : %-30.2f |%n", "Subtotal", subtotal);
+        System.out.format("| %-17s : %-30s |%n", "Tax", "2.5% (" + Math.round(tax * 100.0) / 100.0 + ")");
+        System.out.println("+----------------------------------------------------+");
+        System.out.format("| %-17s : %-30.2f |%n", "Total", total);
+        System.out.println("+----------------------------------------------------+\n");
     }
 }
